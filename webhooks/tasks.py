60cf24ee2c18706a69462c243c1c771229be2025
sunilgaru1 <sunilgaru097@gmail.com>
2026-06-28 16:52:20 +0530

    Added Check for celery's first task check 2

--- webhooks/tasks.py
+++ webhooks/tasks.py
@@ -2,4 +2,4 @@ from celery import shared_task
 
 @shared_task        #Celery registers this function as a task.
 def test_task():
-    return "Celery is working!"
+    print("Celery task executed successfully!")
